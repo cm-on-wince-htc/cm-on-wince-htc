8b3d37d9202f5fb5861cc94b9c1a5efccf5e592f
Steve Kondik <shade@chemlab.org>
2010-10-16 04:10:44 -0400

    Revert "yaffs: Cache object xattrib state."
    This reverts commit a1bcefa40c4b9c5db928ba38326c41fe44290529.

--- fs/yaffs2/yaffs_nameval.c
+++ fs/yaffs2/yaffs_nameval.c
@@ -190,7 +190,6 @@ int nval_list(const char *xb, int xb_size, char *buf, int bsize)
 	return ncopied;
 }
 
-#if 0
 int nval_load(char *xb, int xb_size, const char *src, int src_size)
 {
 	int tx_size;
@@ -218,11 +217,4 @@ int nval_save(const char *xb, int xb_size, char *dest, int dest_size)
 
 	memcpy(dest,xb,tx_size);
 	return tx_size;
-}
-#endif
-
-
-int nval_hasvalues(const char *xb, int xb_size)
-{
-	return nval_used(xb, xb_size) > 0;
 }
